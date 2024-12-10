b73192305190caa447bfa96966d9322acf4952f2
jodarini <rivasjose9428@gmail.com>
2024-12-10 15:42:42 -0500

    feat: save user to localstorage

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,9 +1,48 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { FormEvent, useState } from "react";
 
-export const Route = createFileRoute('/sign-up')({
+export const Route = createFileRoute("/sign-up")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/sign-up"!</div>
+  const [username, setUsername] = useState("");
+
+  const signUp = (newUsername: string) => {
+    const storedUsers = localStorage.getItem("users");
+
+    const users: string[] = storedUsers ? JSON.parse(storedUsers) : [];
+
+    if (users.some((user) => user === newUsername)) {
+      alert("User already exists");
+      return;
+    }
+
+    users.push(newUsername.trim());
+
+    localStorage.setItem("users", JSON.stringify(users));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signUp(username);
+  };
+
+  return (
+    <div>
+      Hello "/sign-up"!
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+      {username}
+    </div>
+  );
 }
