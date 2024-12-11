55c8ef3346bb3ae24acce0fb7fb5e0278d843ed7
jodarini <rivasjose9428@gmail.com>
2024-12-11 18:58:06 -0500

    refactor: remove unused imports

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { FormEvent, useState } from "react";
 import { useAuth } from "../auth";
 
